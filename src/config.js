12800cd02a77535fc4a6b5cbe8f25589e89ebf1b
Burl Adkins <adkins1393@gmail.com>
2019-07-31 15:26:32 -0400

    environment variables

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$rGsklk64cjByX.MPxWO5YukQGBk4BpkrIB1L0ZgWpKbDmWqsOKcte',
+  API_KEY: process.env.REACT_APP_KEY,
 }
